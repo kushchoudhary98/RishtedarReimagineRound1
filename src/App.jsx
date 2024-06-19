8260463ea4a923e46f2088e97f31a3a6c7177ef6
kushchoudhary98 <kushchoudhary8430@gmail.com>
2024-06-19 14:14:46 +0530

    [Added]: Lenis Smooth Scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import Features from "./sections/Features"
+import { ReactLenis, useLenis } from 'lenis/react'
 import Hero from "./sections/Hero"
 import Navbar from "./sections/Navbar"
 
 function App() {
 
   return (
+    <ReactLenis root>
     <div>
       <Navbar />
       <Hero />
     </div>
+    </ReactLenis>
   )
 }
 
