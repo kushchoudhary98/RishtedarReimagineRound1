eb9a9a78d239b89c383588270e8e3be1a4bed810
kushchoudhary98 <kushchoudhary8430@gmail.com>
2024-06-22 19:41:09 +0530

    [Added]: NavFlyout"

--- src/components/NavFlyout.jsx
+++ src/components/NavFlyout.jsx
@@ -1,32 +1,31 @@
 import { motion, AnimatePresence } from 'framer-motion'
 import { useState } from 'react';
 
-const NavFlyout = ({ children, href, FlyoutContent }) => {
+const NavFlyout = ({ children, FlyoutContent }) => {
     const [open, setOpen] = useState(false);
 
     const showFlyout = FlyoutContent && open;
 
     return (
         <div
-            onMouseEnter={() => setOpen(true)}
-            onMouseLeave={() => setOpen(false)}
+            onClick={()=>{setOpen(!open)}}
             className="relative w-fit h-fit"
         >
-            <a href={href} className="relative text-zinc-400 hover:text-white">
+            <button className="relative de">
                 {children}
                 <span
                     style={{
                         transform: showFlyout ? "scaleX(1)" : "scaleX(0)",
                     }}
-                    className="absolute -bottom-2 -left-2 -right-2 h-1 origin-left scale-x-0 rounded-full bg-green-600 transition-transform duration-300 ease-out"
+                    className="absolute -bottom-2 -left-2 -right-2 h-1 origin-left rounded-full bg-black transition-transform duration-300 ease-out"
                 />
-            </a>
+            </button>
             <AnimatePresence>
                 {showFlyout && (
                     <motion.div
-                        initial={{ opacity: 0, y: 15 }}
+                        initial={{ opacity: 0, y: -15 }}
                         animate={{ opacity: 1, y: 0 }}
-                        exit={{ opacity: 0, y: 15 }}
+                        exit={{ opacity: 0, y: -15 }}
                         style={{ translateX: "-50%" }}
                         transition={{ duration: 0.3, ease: "easeOut" }}
                         className="absolute left-1/2 top-12 bg-white text-black"
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -2,9 +2,63 @@ import logo from '../assets/logo.svg'
 import cart_icon from '../assets/cart.svg'
 import search_icon from '../assets/search.svg'
 import hamburger_icon from '../assets/hamburger.svg'
-import { motion } from 'framer-motion'
+import { easeInOut, motion } from 'framer-motion'
+import { useState } from 'react'
+import NavFlyout from '../components/NavFlyout'
+import whyArettoPic from '../assets/nav/why_aretto.svg'
+import sizePic from '../assets/nav/size.png'
+import blogPic from '../assets/nav/blog.webp'
+import pressPic from '../assets/nav/press.png'
+import defaultPic from '../assets/nav/default.png'
 
 export default function Navbar() {
+
+  const [currIndex, setCurrIndex] = useState(0);
+  const [prevIndex, setPrevIndex] = useState(0);
+
+  const setIndex = (num) => {
+    setPrevIndex(currIndex);
+    setCurrIndex(num);
+  }
+
+  const flyoutContent = () => {
+    return (
+      <div className="w-[500px] h-[204px] bg-white shadow-lg rounded-xl flex flex-row justify-between gap-5">
+        <div className='p-6 font-inter text-base flex flex-col gap-5'>
+          <a href="#" onMouseEnter={(e) => { e.preventDefault(); setIndex(1); }} className="hover:underline">
+            Why Aretto
+          </a>
+          <a href="#" onMouseEnter={(e) => { e.preventDefault(); setIndex(2); }} className="hover:underline">
+            Size Guide
+          </a>
+          <a href="#" onMouseEnter={(e) => { e.preventDefault(); setIndex(3); }} className="hover:underline">
+            Blogs
+          </a>
+          <a href="#" onMouseEnter={(e) => { e.preventDefault(); setIndex(4); }} className="hover:underline">
+            In the Press
+          </a>
+        </div>
+        <div className='w-[300px] h-full overflow-hidden'>
+          <motion.div
+            initial={{ y: -prevIndex * 204 }}
+            animate={{ y: -currIndex * 204 }}
+            transition={{
+              ease: easeInOut,
+              duration: 1
+            }}
+            className='h-[400%] w-full'
+          >
+            <img className='h-[204px] w-full object-cover' src={defaultPic} />
+            <img className='h-[204px] w-full object-cover' src={whyArettoPic} />
+            <img className='h-[204px] w-full object-cover' src={sizePic} />
+            <img className='h-[204px] w-full object-cover' src={blogPic} />
+            <img className='h-[204px] w-full object-cover' src={pressPic} />
+          </motion.div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <nav className='flex items-center justify-center'>
       <div className='flex items-center justify-between w-full z-50'>
@@ -14,7 +68,9 @@ export default function Navbar() {
             <a href="#">Home</a>
             <a href="#">New Launch</a>
             <a href="#">Store</a>
-            <button>More links +</button>
+            <NavFlyout href="#" FlyoutContent={flyoutContent} >
+              More links +
+            </NavFlyout>
           </div>
         </div>
         <div className='flex justify-center items-center m-6 md:mx-10 gap-7 md:gap-10'>
@@ -26,7 +82,7 @@ export default function Navbar() {
       <motion.div
         id='nav-flyout'
         className='absolute top-[10vh]'>
-          
+
       </motion.div>
     </nav>
   )
